7f1161075ee38675f4dfc0d4d36cf08e4cd3653e
Tyler Nakamura <me@tylernakamura.com>
2019-11-06 17:47:59 -0600

    fixes to build file for building on macos

--- docker_build.sh
+++ docker_build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# fail when any one command fails
+set -e
+
 IMAGE_TAG=gcr.io/tyrionlannister-237214/dns-hammer
 
-go build dnshammer.go
+env GOOS=linux GOARCH=amd64 go build dnshammer.go
 docker build -t $IMAGE_TAG:latest .
 docker push $IMAGE_TAG:latest
